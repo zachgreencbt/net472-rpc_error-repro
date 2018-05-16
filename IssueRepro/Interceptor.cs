e613c11115662f9e06d4ec04b568fec87e10f6be
Zach Green <zgreen@chainbridgetech.com>
2018-05-16 13:16:11 -0600

    Initial load

---
+++ IssueRepro/Interceptor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data.Common;
+using System.Data.Entity.Infrastructure.Interception;
+using System.Diagnostics;
+
+namespace IssueRepro
+{
+    public class Interceptor : IDbCommandInterceptor
+        {
+
+            public void NonQueryExecuting(DbCommand command, DbCommandInterceptionContext<int> interceptionContext)
+            {
+            }
+
+            public void NonQueryExecuted(DbCommand command, DbCommandInterceptionContext<int> interceptionContext)
+            {
+                Log(command, interceptionContext);
+            }
+
+            public void ReaderExecuting(DbCommand command, DbCommandInterceptionContext<DbDataReader> interceptionContext)
+            {
+            }
+
+            public void ReaderExecuted(DbCommand command, DbCommandInterceptionContext<DbDataReader> interceptionContext)
+            {
+                Log(command, interceptionContext);
+            }
+
+            public void ScalarExecuting(DbCommand command, DbCommandInterceptionContext<object> interceptionContext)
+            {
+            }
+
+            public void ScalarExecuted(DbCommand command, DbCommandInterceptionContext<object> interceptionContext)
+            {
+                Log(command, interceptionContext);
+            }
+
+            private static void Log<T>(DbCommand command, DbCommandInterceptionContext<T> interceptionContext)
+            {
+                 Debug.WriteLine(string.Format(
+                    "Context '{0}' is executing command '{1}'{2}",
+                    interceptionContext.GetType().Name,
+                    command.CommandText.Replace(Environment.NewLine, ""),
+                    Environment.NewLine));
+            }
+
+        }
+}
---
+++ IssueRepro/Program.cs
@@ -0,0 +1,62 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure.Interception;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IssueRepro
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DbInterception.Add(new Interceptor());
+            Task.Run(async () =>
+            {
+                try
+                {
+                    var data = await GetLocalDataAsync(1, 0);
+                    Console.WriteLine("It worked!!");
+                } catch(Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            });
+            Console.ReadLine();
+            return;
+        }
+
+        public delegate IQueryable<TResult> Filter<TModel, TResult>(TModel model, IQueryable<TResult> query);
+
+
+
+        public static async Task<List<Person_WithMoreInfo>> GetLocalDataAsync(int companyId, int arg1)
+        {
+            using (var db = new SimplifiedModelEntities())
+            {
+                return await FetchAndFilterAsync(GetPersonQueryableForList(db, companyId));
+            }
+        }
+
+        public static async Task<List<Person_WithMoreInfo>> FetchAndFilterAsync(IQueryable<Person_WithMoreInfo> query)
+        {
+            return await query
+                 .OrderBy(x => x.Name)
+                 .ToListAsync();
+        }
+
+        private static IQueryable<Person_WithMoreInfo> GetPersonQueryableForList(SimplifiedModelEntities db, int companyId)
+        {
+            var query = from a in db.Person_WithMoreInfo
+                        join b in db.Person_GetForCompany(companyId) on a.Person_Id equals b.Person_Id
+                        select a;
+
+
+            query = query.Where(fs => db.Companies.Any(x => x.Company_Id == fs.Company_Id));
+
+            return query;
+        }
+    }
+}
